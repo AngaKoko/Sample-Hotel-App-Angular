7f809746a77baf44f88245b10756ed76c047147c
angakoko <angakoko@gmail.com>
2026-01-14 15:07:20 +0100

    Linking from home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeModule } from './home-module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeModule],
+  imports: [HomeModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
