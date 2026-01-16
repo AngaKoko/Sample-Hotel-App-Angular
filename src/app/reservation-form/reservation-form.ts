69dc4311f1ed4b8bb5226a733222d74f74c2fa68
angakoko <angakoko@gmail.com>
2026-01-16 09:41:36 +0100

    Create reservation form

--- src/app/reservation-form/reservation-form.ts
+++ src/app/reservation-form/reservation-form.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ReservationModule } from '../reservation/reservation-module';
+import { FormGroup, FormBuilder, Validator, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './reservation-form.html',
   styleUrl: './reservation-form.css',
 })
 export class ReservationFormCompoment {
 
+  reservationForm : FormGroup = new FormGroup({})
+
+  onSubmit(){
+    
+  }
 }
